de34a667546d34846251afbf3db33e40377e7c73
mustapha <mofodev01@gmail.com>
2020-01-25 16:28:47 +0100

    change-8

--- cordovaOverride.sh
+++ cordovaOverride.sh
@@ -3,7 +3,9 @@ set -o errexit #exit with error code if something goes wrong.
 if [ -z ${CI_APP_NAME} ]; then
 echo "Not in Appflow skipping cordova hack";
 else
-echo "In Appflow...using 8.1.2 not  cordova@latest"
+echo "In Appflow...using 8.1.2"
 npm uninstall -g @ionic-enterprise/cordova
 npm install -g cordova@8.1.2
+echo "In Appflow...using cordova-ios@5.1.1"
+npm i cordova-ios@5.1.1
 fi
